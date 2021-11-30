5a1b1195bae46b84300e215401967f210ac56df0
Kaikeeksr <82270498+Kaikeeksr@users.noreply.github.com>
2021-11-29 22:14:33 -0300

    Update tabelas.sql

--- tabelas.sql
+++ tabelas.sql
@@ -16,39 +16,39 @@ USE CINETEC3;
 
 CREATE TABLE tbl_genero(
 	id_genero int primary key auto_increment,
-    genero varchar(45) not null
+    	genero varchar(45) not null
 );
 
 CREATE TABLE tbl_ator(
 	id_ator int primary key auto_increment,
-    nome_ator varchar(45) not null,
-    sexo varchar(1) not null,
-    dt_nascimento date not null
+    	nome_ator varchar(45) not null,
+    	sexo varchar(1) not null,
+    	dt_nascimento date not null
 );
 
 CREATE TABLE tbl_diretor(
 	id_diretor int primary key auto_increment,
-    nome_diretor varchar(45) not null,
-    nacionalidade varchar(10) not null,
-    dt_nascimento date not null
+    	nome_diretor varchar(45) not null,
+    	nacionalidade varchar(10) not null,
+    	dt_nascimento date not null
 );
 
 CREATE TABLE tbl_filme(
 	id_filme int primary key auto_increment,
-    nome_filme_pt varchar(45) not null,
-    nome_filme_en varchar(45),
-    ano_lancamento int(4) not null,
-    duracao int(3) not null,
-    FK_id_genero int not null,
-    FK_id_diretor int not null,
-    constraint FK_id_genero foreign key(FK_id_genero) references tbl_genero(id_genero),
+    	nome_filme_pt varchar(45) not null,
+    	nome_filme_en varchar(45),
+    	ano_lancamento int(4) not null,
+    	duracao int(3) not null,
+    	FK_id_genero int not null,
+    	FK_id_diretor int not null,
+    	constraint FK_id_genero foreign key(FK_id_genero) references tbl_genero(id_genero),
 	constraint FK_id_diretor foreign key(FK_id_diretor) references tbl_diretor(id_diretor)
 );
 
 CREATE TABLE tbl_filme_has_tbl_ator(
 	FK_id_filme int not null,
-    FK_id_ator int not null,
-    constraint FK_id_filme foreign key(FK_id_filme) references tbl_filme(id_filme),
-    constraint FK_id_ator foreign key(FK_id_ator) references tbl_ator(id_ator),
-    primary key(FK_id_filme, FK_id_ator)
+    	FK_id_ator int not null,
+    	constraint FK_id_filme foreign key(FK_id_filme) references tbl_filme(id_filme),
+    	constraint FK_id_ator foreign key(FK_id_ator) references tbl_ator(id_ator),
+    	primary key(FK_id_filme, FK_id_ator)
 );
